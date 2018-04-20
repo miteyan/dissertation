51a9d8346997993680428dfe66d890b29db85b5f
miteyan <miteyan25@gmail.com>
2018-04-20 14:17:37 +0100

    Added roc curves

---
+++ project/classifiers/models/ROC.py
@@ -0,0 +1,147 @@
+import numpy as np
+from scipy import interp
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_curve, auc
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.model_selection import StratifiedKFold
+import dataset_tools.dataset_functions as ds
+
+dataset1 = "/var/storage/miteyan/Dissertation/project/data/emotion_sense_dataset.csv"
+
+
+cv = StratifiedKFold(n_splits=4)
+classifier = DecisionTreeClassifier()
+dataset = ds.get_all_data(dataset1, 0.00)
+y, X = ds.get_labels_and_features(dataset)
+n_samples, n_features = X.shape
+
+tprs = []
+aucs = []
+mean_fpr = np.linspace(0, 1, 100)
+
+i = 0
+for train, test in cv.split(X, y):
+    probas_ = classifier.fit(X[train], y[train]).predict_proba(X[test])
+    # Compute ROC curve and area the curve
+    fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+    tprs.append(interp(mean_fpr, fpr, tpr))
+    tprs[-1][0] = 0.0
+    roc_auc = auc(fpr, tpr)
+    aucs.append(roc_auc)
+
+mean_tpr = np.mean(tprs, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+std_auc = np.std(aucs)
+plt.plot(mean_fpr, mean_tpr, color='b',
+         label=r'Mean ROC EmotionSense Decision Tree (%0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+         lw=2, alpha=.8)
+
+std_tpr = np.std(tprs, axis=0)
+
+cv = StratifiedKFold(n_splits=4)
+classifier = RandomForestClassifier(max_depth=6, max_features=5, min_samples_leaf=6)
+dataset = ds.get_all_data(dataset1, 0.00)
+y, X = ds.get_labels_and_features(dataset)
+n_samples, n_features = X.shape
+
+tprs = []
+aucs = []
+mean_fpr = np.linspace(0, 1, 100)
+
+i = 0
+for train, test in cv.split(X, y):
+    probas_ = classifier.fit(X[train], y[train]).predict_proba(X[test])
+    # Compute ROC curve and area the curve
+    fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+    tprs.append(interp(mean_fpr, fpr, tpr))
+    tprs[-1][0] = 0.0
+    roc_auc = auc(fpr, tpr)
+    aucs.append(roc_auc)
+
+mean_tpr = np.mean(tprs, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+std_auc = np.std(aucs)
+plt.plot(mean_fpr, mean_tpr, color='y',
+         label=r'Mean ROC Random Forest (%0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+         lw=2, alpha=.8)
+
+std_tpr = np.std(tprs, axis=0)
+
+from sklearn.neural_network import MLPClassifier
+cv = StratifiedKFold(n_splits=4)
+classifier = MLPClassifier()
+dataset = ds.get_all_data(dataset1, 0.00)
+y, X = ds.get_labels_and_features(dataset)
+n_samples, n_features = X.shape
+
+tprs = []
+aucs = []
+mean_fpr = np.linspace(0, 1, 100)
+
+i = 0
+for train, test in cv.split(X, y):
+    probas_ = classifier.fit(X[train], y[train]).predict_proba(X[test])
+    # Compute ROC curve and area the curve
+    fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+    tprs.append(interp(mean_fpr, fpr, tpr))
+    tprs[-1][0] = 0.0
+    roc_auc = auc(fpr, tpr)
+    aucs.append(roc_auc)
+
+mean_tpr = np.mean(tprs, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+std_auc = np.std(aucs)
+plt.plot(mean_fpr, mean_tpr, color='c',
+         label=r'Mean ROC Neural Network (%0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+         lw=2, alpha=.8)
+
+std_tpr = np.std(tprs, axis=0)
+
+
+cv = StratifiedKFold(n_splits=4)
+classifier = BernoulliNB()
+dataset = ds.get_all_data(dataset1, 0.00)
+y, X = ds.get_labels_and_features(dataset)
+n_samples, n_features = X.shape
+
+tprs = []
+aucs = []
+mean_fpr = np.linspace(0, 1, 100)
+
+i = 0
+for train, test in cv.split(X, y):
+    probas_ = classifier.fit(X[train], y[train]).predict_proba(X[test])
+    # Compute ROC curve and area the curve
+    fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+    tprs.append(interp(mean_fpr, fpr, tpr))
+    tprs[-1][0] = 0.0
+    roc_auc = auc(fpr, tpr)
+    aucs.append(roc_auc)
+    i += 1
+
+mean_tpr = np.mean(tprs, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+std_auc = np.std(aucs)
+plt.plot(mean_fpr, mean_tpr, color='g',
+         label=r'Mean ROC Naive Bayes (%0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+         lw=2, alpha=.8)
+
+std_tpr = np.std(tprs, axis=0)
+tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+         label='Random', alpha=.8)
+
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic Curve')
+plt.legend(loc="lower right")
+plt.show()
